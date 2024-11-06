b7d085a7128166bf2ee07da1b419952ee0832e62
Dhiren Mathur <dhiru17398@gmail.com>
2024-11-06 18:41:35 +0530

    add reply_to in email (#171)

--- app/modules/utils/email_helper.py
+++ app/modules/utils/email_helper.py
@@ -9,7 +9,7 @@ class EmailHelper:
         self.transaction_emails_enabled = (
             os.environ.get("TRANSACTION_EMAILS_ENABLED", "false").lower() == "true"
         )
-        self.from_address = os.environ.get("EMAIL_FROM_ADDRESS", "hi@potpie.ai")
+        self.from_address = os.environ.get("EMAIL_FROM_ADDRESS", "dhiren@updates.potpie.ai")
         resend.api_key = self.api_key
 
     async def send_email(self, to_address, repo_name, branch_name ):
@@ -17,18 +17,19 @@ class EmailHelper:
             return
 
         params = {
-            "from": self.from_address,
+            "from": f"Dhiren Mathur <{self.from_address}>",
             "to": to_address,
             "subject": f"Your repository {repo_name} is ready! 🥧",
+            "reply_to": "dhiren@potpie.ai",
             "html": f"""
                 <p>Hi!</p>
-                <p>Your repo <strong>{repo_name}</strong> at branch <strong>{branch_name}</strong> has been processed successfully.</p>
+                <p>Your repository <strong>{repo_name}</strong> at branch <strong>{branch_name}</strong> has been processed successfully.</p>
                 <p>You can use any of Potpie's ready-to-use agents to chat with it at: 
                     <a href='https://app.potpie.ai/newchat'>https://app.potpie.ai/newchat</a>.
                 </p>
                 <p>Please refer this document to get started: <a href='https://potpieai.notion.site/potpie-s-beta-program-10cc13a23aa8801e8e2bd34d8f1488f5'>Potpie User Guide</a></p>
-                <p>Reply to this email or reach out to the founders at <a href='mailto:dhiren@potpie.ai'>dhiren@potpie.ai</a> if you have any questions.</p>
-                <p>Thanks, <br />The Potpie Team 🥧</p>
+                <p>Feel free to reply to this email if you have any questions.</p>
+                <p>Thanks, <br />Dhiren Mathur,<br /> Co-Founder, Potpie 🥧</p>
             """,
         }
 
